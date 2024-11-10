df4a14a3b3fa798e626b557f931cc2896c271d07
BnRamadan <medoramadan856@gmail.com>
2024-11-10 04:43:58 +0200

    third fe

---
+++ script.js
@@ -0,0 +1,28 @@
+// ! 🔥 💪🏻 Bn Ramadan frontend project 3 ( portfolio website ) 💪🏻 🔥
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// ! " The end of this project is the completing of future successes and creativity , Just strive, be patient, commit and keep going. "
+// !    🔥 💪🏻 Always forward! 💪🏻 🔥
